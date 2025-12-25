681b728a426e2dfea3824a3a4a3e44c9cd70ecfc
archana781 <67378304+archana781@users.noreply.github.com>
2025-12-25 17:21:02 +0530

    Update SampleApiTest.java

--- src/test/java/api/SampleApiTest.java
+++ src/test/java/api/SampleApiTest.java
@@ -1,1 +1,17 @@
+package api;
 
+import static io.restassured.RestAssured.*;
+import org.testng.annotations.Test;
+
+public class SampleApiTest {
+
+    @Test
+    public void getUser() {
+        given()
+            .baseUri("https://reqres.in")
+        .when()
+            .get("/api/users/2")
+        .then()
+            .statusCode(200);
+    }
+}
